602c865f49cf530a8212c80fd5eabc42dd7ca7c9
ChuckHagy <chuckhagy@gmail.com>
2017-11-28 11:57:38 -0800

    new seed data

--- Seeds/001-User.js
+++ Seeds/001-User.js
@@ -4,6 +4,8 @@ exports.seed = function(knex, Promise) {
         .then(function () {
             return knex('User').insert([
                 {email: 'chuckhagy@gmail.com', hashedPassword: '$2a$10$sMcezAa4d2XsUD/uAS41te5tVH0qeE4z9VoR.bv5bP7T.EfFfUAdm'},
+                {email: 'melisaim@gmail.com', hashedPassword: '$2a$10$sMcezAa4d2XsUD/uAS41te5tVH0qeE4z9VoR.bv5bP7T.EfFfUAdm'},
+                {email: 'jasonhsu@gmail.com', hashedPassword: '$2a$10$sMcezAa4d2XsUD/uAS41te5tVH0qeE4z9VoR.bv5bP7T.EfFfUAdm'},
             ]);
         });
 };
